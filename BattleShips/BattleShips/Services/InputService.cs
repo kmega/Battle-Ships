812f6656120070e22ede6b578f6d2b8b97241b51
Karol Szelążek <charles.sz@protonmail.com>
2019-08-14 13:38:21 +0200

    Created InputService for players input. Added additional comments.

---
+++ BattleShips/BattleShips/Services/InputService.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace BattleShips
+{
+    internal class InputService
+    {
+        internal string GetPlayerInput()
+        {
+            return Console.ReadLine();
+        }
+
+        internal void CheckPlayerInput(string playerInput, Cell[,] boardHolder)
+        {
+            CheckBoardBoundaries();
+
+            throw new NotImplementedException();
+        }
+    }
+}
--- BattleShips/BattleShips/Services/ShipService.cs
+++ BattleShips/BattleShips/Services/ShipService.cs
@@ -6,10 +6,33 @@ namespace BattleShips
     {
         internal void PlaceShips(ref Cell[,] playerOneBoard, ref Cell[,] playerTwoBoard)
         {
-            // Create UserInterface() and show players boards for ship placement.
+            // Create UserInterface(), show players their boards and let them place their ships.
             UserInterface userInterface = new UserInterface();
 
-            userInterface.ShowBoard(playerOneBoard);
+            Cell[,] boardHolder;
+            bool playerOneTurn = true;
+
+            while (true)
+            {
+                if (playerOneTurn == true)
+                {
+                    userInterface.ShowBoard(playerOneBoard);
+
+                    boardHolder = playerOneBoard;
+                }
+                else
+                {
+                    userInterface.ShowBoard(playerTwoBoard);
+
+                    boardHolder = playerTwoBoard;
+                }
+
+                InputService inputService = new InputService();
+
+                string playerInput = inputService.GetPlayerInput();
+
+                inputService.CheckPlayerInput(playerInput, boardHolder);
+            }
 
             throw new NotImplementedException();
         }
